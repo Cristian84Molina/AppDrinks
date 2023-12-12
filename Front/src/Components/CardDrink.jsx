ee4550eddc066f3abf4ab5323b88a268b5852467
Cristian <cristian84molina@gmail.com>
2023-12-12 20:06:55 -0300

    first commit

---
+++ Front/src/Components/CardDrink.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+
+function CardDrink ({data}) {
+
+
+    return(
+        <div>
+
+        </div>
+    )
+
+
+}
--- Front/src/Views/AdminDrinks.jsx
+++ Front/src/Views/AdminDrinks.jsx
@@ -9,7 +9,6 @@ const AdminDrinks = () =>{
         <div>
             <NavBarAdmin />
             <SideBarAmin />
-            <Drinks />
         </div>
     )
 
