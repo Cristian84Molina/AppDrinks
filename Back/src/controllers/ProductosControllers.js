15c905c7dc146f1800c1336b1006262dca09944f
pol <polmn98@gmail.com>
2023-12-11 19:36:10 -0500

    Update productos

--- Back/src/controllers/ProductosControllers.js
+++ Back/src/controllers/ProductosControllers.js
@@ -11,10 +11,21 @@ const getProductosAll = async() => {
 
 const addProducto = async(datos) => {
    const {name, image, active, preciocosto, precioventa, impuesto, preparacion, linea_id} = datos;
-   if(!name || !active || !preciocosto || !precioventa || !preparacion || !linea_id ) {
-      throw Error("Datos Incompletos");
+   if(!name || !linea_id ) {
+      throw Error("Datos Básicos Incompletos");
    };
-   const result = await productos.create(datos);
+   const newReg = {
+       name,
+       image: image ?image : null,
+       active: active ?active : 1,
+       preciocosto: preciocosto ?preciocosto : 0,
+       precioventa: precioventa ?precioventa : 0,
+       impuesto: impuesto ?impuesto : 0,
+       preparacion: preparacion ?preparacion : null,
+       linea_id
+   };
+ 
+   const result = await productos.create(newReg);
    return result;
 };
 
@@ -35,7 +46,28 @@ const getProductosById = async(id) => {
    return array;
 };
 
+const updateProducto = async(datos, id) => {
+   const {name, image, active, preciocosto, precioventa, impuesto, preparacion, linea_id} = datos;
+   if(!name || !linea_id  || !id) {
+      throw Error("Datos Básicos Incompletos");
+   };
+   const idreg = Number(id);
+   const newReg = {
+       name,
+       image: image ?image : null,
+       active: active ?active : 1,
+       preciocosto: preciocosto ?preciocosto : 0,
+       precioventa: precioventa ?precioventa : 0,
+       impuesto: impuesto ?impuesto : 0,
+       preparacion: preparacion ?preparacion : null,
+       linea_id
+   };  
+   const registro = productos.update(newReg, {where: {id: idreg}});
+   return registro; 
+};
+
 module.exports = {getProductosAll, 
                   addProducto,
                   getProductosById,
-                  getProductosxLinea};
+                  getProductosxLinea,
+                  updateProducto};
