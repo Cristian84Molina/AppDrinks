5636d8b2cbb3fd12c4fa57151ca918674e4d1543
Cristian <cristian84molina@gmail.com>
2023-12-15 20:05:04 -0300

    edit Drink y New drink

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './Views/Login'
 import Admin from './Views/Admin'
 import Home from "./Views/Home";
 import AdminDrinks from './Views/AdminDrinks';
+import AdminNewDrinks from './Views/AdminNewDrinks';
 
 
 
@@ -16,7 +17,8 @@ function App() {
       <Route exact path='/' element={<Login />}/>
       <Route exact path ='/admin' element={<Admin />} />
       <Route exact path="/home" element={<Home />} />
-      <Route exact path="/admin/drinks" element={<AdminDrinks />} />
+      <Route exact path="/admin/drinks" element={<Admin />} />
+      <Route exact path='/admin/NewDrinks' element={<AdminNewDrinks />} />
       
     </Routes>
   )
--- Front/src/Components/CardDrink.jsx
+++ Front/src/Components/CardDrink.jsx
@@ -17,8 +17,8 @@ const CardDrink = () => {
   }, []);
 
   return (
-    <div className="container mx-auto my-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="container mx-2 my-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
         {productos.map((producto) => (
           <div
             key={producto.id}
